5b5ae7ca2c7df14157a7283214c9eed390d25598
Noragiri <33696866+Noragiri@users.noreply.github.com>
2024-10-01 12:20:52 +0200

    fix sleep_in / organize files

--- Selfstudy/CodingBat/sleep_in.py
+++ Selfstudy/CodingBat/sleep_in.py
@@ -1,12 +1,8 @@
-def count_code(str):
-    i = 0
-    count = 0
-    while i < len(str) - 2:
-        if str[i] == "o" and str[i - 1] == "c" and str[i + 2] == "e":
-            count += count
-        i += 1
-
-    return count
-
-
-print(count_code("codexxcode"))
+def sleep_in(weekday, vacation):
+    if weekday == False:
+        return True
+    if weekday == True:
+        if vacation == True:
+            return True
+        else:
+            return False
